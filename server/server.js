a5b2842639bbe519ec3ddbd1e82ea56ee075a03a
Jia <jovsofjdo@gmail.com>
2023-09-06 10:36:16 -0700

    Added routes to server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
+import cors from "cors";
+import express from "express";
 import "dotenv/config";
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.port || 8080;
+const APIKEY = process.env.APIKEY;
+
+app.get(() => {});
+
+app.get(() => {});
+
+app.listen(PORT, () => {
+  console.log(`Your man is listening on ${PORT}`);
+});
