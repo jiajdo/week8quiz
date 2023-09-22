0a2e7ea4e6e0820075833512ce75fb1b922e40cc
Jia <jovsofjdo@gmail.com>
2023-09-22 06:00:46 -0700

    Added API link to backend

--- server/server.js
+++ server/server.js
@@ -8,9 +8,20 @@ app.use(cors());
 const PORT = process.env.port || 8080;
 const APIKEY = process.env.API_KEY;
 
-app.get(() => {});
+app.get('/', (req, res) => {
+  res.json({ message: "Test" });
+});
 
-app.get(() => {});
+app.get('/quiz', (req, res) => {
+  fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=boolean')
+  .then(response => {
+  return response.json()
+  })
+  .then(apiData => {
+  res.json(apiData);
+  })
+});
+
 
 app.listen(PORT, () => {
   console.log(`Your man is listening on ${PORT}`);
