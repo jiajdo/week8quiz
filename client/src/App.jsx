061792d5dd26b376c63299c429c1f069c7c29678
Jia <jovsofjdo@gmail.com>
2023-09-22 06:22:35 -0700

    Added getData and useEffect funcs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Card from "./components/Card";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const response = await fetch("http://localhost:8080/quiz");
+    const responseData = await response.json();
+    console.log(responseData.results);
+    setData(responseData.results);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
-    <div className="app">
+    <>
+      <h1>Take the Quiz</h1>
+
       <Card />
-    </div>
+    </>
   );
 };
 
