05307094c76f4b85d1fe41c2367396cdf2be70a2
Jia <jovsofjdo@gmail.com>
2023-09-22 18:06:19 -0700

    Added new component structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,22 @@
 import { useState, useEffect } from "react";
 import "./App.css";
-// import Header from './components/header.js';
-// import UserForm from './components/user.js';
-// import Game from './components/game.js';
-
-
+import Header from "./components/Header.jsx";
+import UserForm from "./components/User.jsx";
+import Game from "./components/game";
 
 function App() {
   const [user, setUser] = useState("");
-  const handleUser = (text) =>{
+  const handleUser = (text) => {
     setUser(text);
-  }
+  };
 
   return (
     <div className="App">
-    <Header user={user} />
-    <UserForm grabUser={handleUser} />
-    {user ? <Game /> : null}
-      
+      <Header user={user} />
+      <UserForm grabUser={handleUser} />
+      {user ? <Game /> : null}
     </div>
   );
 }
 
 export default App;
-
-
