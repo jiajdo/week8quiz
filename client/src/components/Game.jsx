a3e9b822c69a9a236f035efb55ee9966c31205ba
Jia <jovsofjdo@gmail.com>
2023-09-22 19:25:05 -0700

    Added functions for final score

--- client/src/components/Game.jsx
+++ client/src/components/Game.jsx
@@ -2,31 +2,46 @@ import { useState, useEffect } from "react";
 import QuestionCard from "./QuestionCard";
 
 const Game = (props) => {
-  const [questions, setQuestions] = useState([]);
 
-  const loadData = () => {
-    fetch("http://localhost:5000/api/game")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("This is line 11", data.results);
-        setQuestions(data.results);
-      });
-  };
+    // Sets questions and score
+    const [questions, setQuestions] = useState([]);
+    const [score, setScore] = useState(0);
 
-  useEffect(() => {
-    loadData();
-  }, []);
+    // Fetches data from API for questions and answers
+    const loadData = () => {
+        fetch('http://localhost:8080/quiz')
+            .then((response) => response.json())
+            .then(data => {
+                setQuestions(data.results);
+            })
+    }
 
-  return (
-    <div className="Container">
-      <div className="question-count">
-        <span>Question 1</span>/{questions.length}
-      </div>
-      {questions.map((question, index) => {
-        return <QuestionCard key={index} question={question} />;
-      })}
-    </div>
-  );
-};
+    useEffect(() => {
+        loadData();
+    }, [])
+
+    // Callback function to get final score
+    const childToParent = (childData) => {
+        setScore(childData);
+    }
+
+    return (
+        <div className="Container">
+            {questions.map((question, index) => (
+                <QuestionCard
+                key={index}
+                question={question}
+                onAnswerSelect={(selectedAnswer) => handleAnswerSelect(selectedAnswer, index)}
+                score={score}
+                setScore={setScore}
+                qNum={index + 1}
+                childToParent={childToParent}
+                />
+            ))}
+            <div>Final Score: {score}!</div>
+        </div>
+    )
+
+}
 
 export default Game;
--- client/src/components/QuestionCard.jsx
+++ client/src/components/QuestionCard.jsx
@@ -1,14 +1,52 @@
+import { useState, useEffect } from "react";
+
 const QuestionCard = (props) => {
-    
-    return (
-      <div className={"question-section"}>
-        <div className='question-text'>{props.question.question}</div>
-        <div className='answer-section'>
-		<button>True</button>
-		<button>False</button>
-        </div>
-      </div>
-    );
+  // Makes a list of answers and places correct answer at a random index
+  const { correct_answer, incorrect_answers } = props.question;
+  const [allOptions, setAllOptions] = useState([]);
+  const getRandomNum = () => {
+    return Math.floor(Math.random() * (allOptions.length + 1));
   };
+  // Makes buttons stay in constant positions
+  useEffect(() => {
+    const finalOptions = [...incorrect_answers];
+    finalOptions.splice(getRandomNum(), 0, correct_answer);
+    setAllOptions(finalOptions);
+  }, [correct_answer, incorrect_answers]);
+
+  // Logs if user selected an answer or not
+  const [answerSelected, setAnswerSelected] = useState(false);
+  // Logs if the selected answer was correct
+  const [selectedAns, setSelectedAns] = useState(null);
+
+  // When user chooses answer, log selected answer
+  const handleBtnClick = (event) => {
+    setAnswerSelected(true);
+    setSelectedAns(event.target.textContent);
+    // If answer is correct, add 1 to score
+    if (event.target.textContent === correct_answer) {
+      props.setScore(props.score + 1);
+      props.childToParent(score);
+    }
+  };
+
+  return (
+    <div className={"question-section"}>
+      <span>Question {props.qNum}</span>/10
+      {/* Shows question formatted for humans */}
+      <div
+        className="question-text"
+        dangerouslySetInnerHTML={{ __html: props.question.question }}
+      ></div>
+      <div className="answer-section">
+        {allOptions.map((option, index) => (
+          <button key={index} onClick={handleBtnClick}>
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default QuestionCard;
